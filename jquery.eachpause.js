85db66e36c5dcdae89ceb93a15709ea9098c4d4a
mcrittenden <mcrittenden@freshbox.(none)>
2010-03-30 22:20:39 -0400

    spacing changes

--- jquery.eachpause.js
+++ jquery.eachpause.js
@@ -5,16 +5,16 @@
  *
  * http://github.com/mikecrittenden/jquery-eachpause
  */
-
 jQuery.fn.eachPause = function(interval, callback) {
   var array = this;
-  if(! array.length) return;
+  if(!array.length) 
+    return;
   var i = 0;
   next();
   function next() {
-    if( callback.call(array[i], i, array[i]) !== false )
+    if(callback.call(array[i], i, array[i]) !== false)
       if(++i < array.length)
-        setTimeout( next, interval );
+        setTimeout(next, interval);
   }
 };
 
